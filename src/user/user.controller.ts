9cebd3f03f761ec6aa4cf026de144f1e656c0af6
joaobarboza <joaolucasbarbozaa@gmail.com>
2023-11-11 12:16:41 -0300

    feat(user): implement `findByEmail`

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
